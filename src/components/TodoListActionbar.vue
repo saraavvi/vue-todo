c8238e2dce99391dc6a6e8107dbe156df50e506b
Sara Viktorsson <saraviktorsson@hotmail.com>
2021-09-17 13:12:39 +0200

    move list actions to its own action bar component

---
+++ src/components/TodoListActionbar.vue
@@ -0,0 +1,61 @@
+<template>
+  <BaseDialog title="Clear all tasks" v-if="isClickedClearAll">
+    <template #default>
+      <p>Are you sure you want to delete all tasks?</p>
+    </template>
+    <template #actions>
+      <BaseButton @click="handleDialogClose" mode="secondary">Close</BaseButton>
+      <BaseButton @click="handleClearAllConfirm" mode="danger">
+        Clear All
+      </BaseButton>
+    </template>
+  </BaseDialog>
+
+  <div class="list-actions-container">
+    <BaseButton mode="primary">FILTER</BaseButton>
+    <BaseButton @click="handleClearAllClick" mode="danger">
+      CLEAR ALL
+    </BaseButton>
+    <TodoListSearch :array="tasks" @searchResult="updateTasksToDisplay" />
+  </div>
+</template>
+
+<script>
+import TodoListSearch from "../components/TodoListSearch";
+export default {
+  components: { TodoListSearch },
+  props: ["tasks"],
+  emits: ["taskClearAll", "updateTasksToDisplay"],
+  data() {
+    return {
+      isClickedClearAll: false,
+    };
+  },
+  methods: {
+    handleClearAllClick() {
+      this.isClickedClearAll = true;
+    },
+    handleDialogClose() {
+      this.isClickedClearAll = false;
+    },
+    handleClearAllConfirm() {
+      this.$emit("taskClearAll");
+      this.isClickedClearAll = false;
+    },
+    updateTasksToDisplay(searchResult) {
+      this.$emit("updateTasksToDisplay", searchResult);
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.list-actions-container {
+  display: flex;
+  margin: 1rem;
+  button,
+  input {
+    margin-right: 5px;
+  }
+}
+</style>
--- src/pages/TodoList.vue
+++ src/pages/TodoList.vue
@@ -1,31 +1,11 @@
 <template>
-  <BaseDialog title="Clear all tasks" v-if="isClickedClearAll">
-    <template #default>
-      <p>Are you sure you want to delete all tasks?</p>
-    </template>
-    <template #actions>
-      <BaseButton @click="handleDialogClose" mode="secondary">Close</BaseButton>
-      <BaseButton @click="handleClearAllConfirm" mode="danger">
-        Clear All
-      </BaseButton>
-    </template>
-  </BaseDialog>
-
-  <TodoListCreate @taskCreate="taskCreate" />
-
-  <!-- container for list and actions -->
+  <TodoListCreateForm @taskCreate="taskCreate" />
   <div class="list-container">
-    <div class="list-actions-container">
-      <!-- <input class="search-field" type="text" placeholder="Search..." /> -->
-      <BaseButton mode="primary">FILTER</BaseButton>
-      <BaseButton @click="handleClearAllClick" mode="danger">
-        CLEAR ALL
-      </BaseButton>
-      <TodoListSearch
-        :array="tasks"
-        @searchResult="updateTasksToDisplay"
-      ></TodoListSearch>
-    </div>
+    <TodoListActionBar
+      :tasks="tasks"
+      @taskClearAll="taskClearAll"
+      @updateTasksToDisplay="updateTasksToDisplay"
+    />
     <ul>
       <TodoListItem
         v-for="item in tasksToDisplay"
@@ -41,32 +21,21 @@
 <script>
 import { MyApi } from "../api/MyApi";
 import TodoListItem from "../components/TodoListItem";
-import TodoListCreate from "../components/TodoListCreate";
-import TodoListSearch from "../components/TodoListSearch";
+import TodoListCreateForm from "../components/TodoListCreateForm";
+import TodoListActionBar from "../components/TodoListActionbar";
 export default {
   components: {
     TodoListItem,
-    TodoListCreate,
-    TodoListSearch,
+    TodoListCreateForm,
+    TodoListActionBar,
   },
   data() {
     return {
-      isClickedClearAll: false,
       tasks: null,
       tasksToDisplay: null,
     };
   },
   methods: {
-    handleClearAllClick() {
-      this.isClickedClearAll = true;
-    },
-    handleDialogClose() {
-      this.isClickedClearAll = false;
-    },
-    handleClearAllConfirm() {
-      this.taskClearAll();
-      this.isClickedClearAll = false;
-    },
     updateTasksToDisplay(searchResult) {
       this.tasksToDisplay = searchResult;
     },
@@ -119,26 +88,6 @@ export default {
     padding: 0;
     margin: 0;
     flex-grow: 1;
-  }
-  .list-actions-container {
-    display: flex;
-    margin: 1rem;
-    button,
-    input {
-      margin-right: 5px;
-    }
-    // .search-field {
-    //   border: 1px solid $secondary;
-    //   border-radius: 5px;
-    //   font-style: cursive;
-    //   padding: 0.6rem 1rem;
-    //   &::placeholder {
-    //     font-style: italic;
-    //   }
-    //   &:focus {
-    //     outline: none;
-    //   }
-    // }
   }
 }
 </style>
